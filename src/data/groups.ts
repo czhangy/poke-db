f676e2738d3bf74c6119f5eb81dbeba2473a67b1
Charles Zhang <charleszhangmb@gmail.com>
2023-10-16 20:00:23 -0700

    Moved files from main project

---
+++ src/data/groups.ts
@@ -0,0 +1,24 @@
+const groups: string[] = [
+    "ruby-sapphire",
+    "emerald",
+    "firered-leafgreen",
+    "diamond-pearl",
+    "platinum",
+    "heartgold-soulsilver",
+    "black-white",
+    "black-2-white-2",
+    "x-y",
+    "omega-ruby-alpha-sapphire",
+    "sun-moon",
+    "ultra-sun-ultra-moon",
+    "lets-go-pikachu-lets-go-eevee",
+    "sword-shield",
+    "isle-of-armor",
+    "crown-tundra",
+    "brilliant-diamond-and-shining-pearl",
+    "legends-arceus",
+    "scarlet-violet",
+    "the-teal-mask",
+];
+
+export default groups;
