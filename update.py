4dbf575a58e89f1a798e024a17b34a28d29317de
Charles Zhang <charleszhangmb@gmail.com>
2023-11-06 17:58:08 -0800

    Refactor update script

--- update.py
+++ update.py
@@ -2,7 +2,6 @@ from colorama import Fore, Style
 import json
 import os
 import requests
-import sys
 import time
 
 # Constants
@@ -17,52 +16,56 @@ POKEMON = "pokemon"
 TRAINERS = "trainers"
 CLEAR = "clear"
 VALID_GROUPS = ["ruby_sapphire", "emerald"]
-
-# Lists
 COLLECTIONS = [ABILITIES, BATTLES, GROUPS, ITEMS, MOVES, POKEMON, TRAINERS, CLEAR]
 
-# Check if server is reachable
-try:
-    page = requests.get(SERVER_URL)
-except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
-    print(f"{Fore.RED}The server is unreachable{Style.RESET_ALL}\n")
-    sys.exit()
 
-# Inputs
-selection = None
-toggled = None
-toggles = [False] * len(COLLECTIONS)
-start = None
-end = None
-battles_group = None
-segments_group = None
-
-# Gather input
-while selection != "":
+# Functions
+def clear_console():
     os.system("cls" if os.name == "nt" else "clear")
 
-    # Print list of options
+
+def check_connection():
+    try:
+        requests.get(SERVER_URL)
+        return True
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
+        print(f"{Fore.RED}The server is unreachable{Style.RESET_ALL}\n")
+        return False
+
+
+def print_options(toggles):
     for idx, collection in enumerate(COLLECTIONS):
         print(
             f"{Fore.GREEN if toggles[idx] else Fore.RED}({collection[0].capitalize()}) {collection.capitalize()}\t[{'✓' if toggles[idx] else ' '}]{Style.RESET_ALL}"
         )
     print()
 
-    # Print status
+
+def print_status(selection, toggled):
     if selection != None and toggled == None:
         print(f"{Fore.RED}Invalid input{Style.RESET_ALL}")
     elif toggled != None:
         print(f"{Fore.YELLOW}{toggled.capitalize()} was toggled{Style.RESET_ALL}")
 
-    # Prompt input
-    toggled = None
-    selection = (
-        input("Pick a field # to toggle or leave empty to run the update: ")
+
+def get_selection():
+    return (
+        input("Pick a field to toggle or leave empty to run the update: ")
         .strip()
         .lower()
     )
 
-    # Toggle selection
+
+def get_group():
+    return input("Pick a group to update: ")
+
+
+def toggle_selection(selection, toggles):
+    start = None
+    end = None
+    battles_group = None
+    segments_group = None
+
     for idx, collection in enumerate(COLLECTIONS):
         if collection[0] == selection:
             if not toggles[idx]:
@@ -74,43 +77,66 @@ while selection != "":
                     if not end.isdigit():
                         break
                 if collection == BATTLES:
-                    battles_group = input("Pick a group to parse: ")
-                    if battles_group not in VALID_GROUPS:
-                        break
+                    battles_group = get_group()
                 if collection == GROUPS:
-                    segments_group = input("Pick a group to update: ")
-                    if segments_group not in VALID_GROUPS:
-                        break
+                    segments_group = get_group()
             toggles[idx] = not toggles[idx]
-            toggled = collection
-            break
+            return collection, start, end, battles_group, segments_group
+    return None
 
-# Construct query
-params = []
-for idx, collection in enumerate(COLLECTIONS):
-    if toggles[idx]:
-        params.append(f"{collection}=true")
-        if collection == POKEMON:
-            params.append(f"pokemon_start={start}")
-        if collection == POKEMON:
-            params.append(f"pokemon_end={end}")
-        if collection == BATTLES:
-            params.append(f"battles_group={battles_group}")
-        if collection == GROUPS:
-            params.append(f"segments_group={segments_group}")
 
-query = SERVER_URL + "?" + "&".join(params)
-print()
+def construct_query(toggles, start, end, battles_group, segments_group):
+    params = []
+    for idx, collection in enumerate(COLLECTIONS):
+        if toggles[idx]:
+            params.append(f"{collection}=true")
+            if collection == POKEMON:
+                params.append(f"pokemon_start={start}")
+            if collection == POKEMON:
+                params.append(f"pokemon_end={end}")
+            if collection == BATTLES:
+                params.append(f"battles_group={battles_group}")
+            if collection == GROUPS:
+                params.append(f"segments_group={segments_group}")
 
-# Make request
-os.system("cls" if os.name == "nt" else "clear")
-start_time = time.perf_counter()
-print(f"{Fore.YELLOW}{Style.BRIGHT}Running update...{Style.RESET_ALL}\n")
-try:
-    r = requests.get(query)
-    print(f"{Fore.CYAN}{json.dumps(json.loads(r.text), indent=2)}{Style.RESET_ALL}\n")
-    print(
-        f"{Fore.GREEN}Update took {Style.BRIGHT}{round(time.perf_counter() - start_time, 1)}{Style.NORMAL} seconds to complete!{Style.RESET_ALL}\n"
-    )
-except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
-    print(f"{Fore.RED}Update failed!{Style.RESET_ALL}\n")
+    print()
+    return SERVER_URL + "?" + "&".join(params)
+
+
+def gather_input():
+    selection = None
+    toggled = None
+    toggles = [False] * len(COLLECTIONS)
+
+    while selection != "":
+        clear_console()
+        print_options(toggles)
+        print_status(selection, toggled)
+        selection = get_selection()
+        res = toggle_selection(selection, toggles)
+        if res:
+            toggled, start, end, battles_group, segments_group = res
+
+    return construct_query(toggles, start, end, battles_group, segments_group)
+
+
+def make_request(query):
+    clear_console()
+    start_time = time.perf_counter()
+    print(f"{Fore.YELLOW}{Style.BRIGHT}Running update...{Style.RESET_ALL}\n")
+    try:
+        r = requests.get(query)
+        print(
+            f"{Fore.CYAN}{json.dumps(json.loads(r.text), indent=2)}{Style.RESET_ALL}\n"
+        )
+        print(
+            f"{Fore.GREEN}Update took {Style.BRIGHT}{round(time.perf_counter() - start_time, 1)}{Style.NORMAL} seconds to complete!{Style.RESET_ALL}\n"
+        )
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
+        print(f"{Fore.RED}Update failed!{Style.RESET_ALL}\n")
+
+
+# Main
+if __name__ == "__main__":
+    if check_connection():
+        make_request(gather_input())
